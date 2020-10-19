a2215cd17c4c57a5e0013584e3137a17e9568412
Gamidi Siva Nagaraju <31251886+sivanagaraju8@users.noreply.github.com>
2020-10-19 21:21:22 +0530

    Add files via upload

---
+++ src/main/java/com/Twitter/auth/WebApplication.java
@@ -0,0 +1,17 @@
+package com.Twitter.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+import com.Twitter.auth.web.TweetController;
+
+@SpringBootApplication
+//@ComponentScan(basePackageClasses=TweetController.class)
+public class WebApplication {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
